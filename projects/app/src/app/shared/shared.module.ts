bfc16dc448865548bf340158111ebb1a712c3af8
Daniel Marin <danielarturomt@gmail.com>
2020-03-20 20:20:00 -0300

    remove tab component for now

--- projects/app/src/app/shared/shared.module.ts
+++ projects/app/src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TabComponent } from './components/tab/tab.component';
 import { TabPanelComponent } from './components/tab-panel/tab-panel.component';
 import { TabTriggerComponent } from './components/tab-trigger/tab-trigger.component';
 
 @NgModule({
-  declarations: [TabComponent, TabPanelComponent, TabTriggerComponent],
+  declarations: [TabPanelComponent, TabTriggerComponent],
   imports: [CommonModule],
-  exports: [TabComponent, TabPanelComponent, TabTriggerComponent]
+  exports: [TabPanelComponent, TabTriggerComponent]
 })
 export class SharedModule {}
